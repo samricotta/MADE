c41be5eff31df412063a592d35e28c9560c88a4b
Sara Foresti <Sara.foresti@hotmail.com>
2018-05-29 12:27:44 +0100

    seeds, added colmuns missing to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :orders
   has_many :meal_reviews
-  has_many :meals
+  has_many :meals, dependent: :destroy
   has_many :meal_orders, through: :orders
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
