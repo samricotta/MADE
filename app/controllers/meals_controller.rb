3da2f1c8f9b98dd9de77e107dd34b7a31b4092f3
Natalia Raffaele <natalia.rafe@gmail.com>
2018-05-29 13:50:01 +0100

    meal index scheleton done

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,11 +1,20 @@
 class MealsController < ApplicationController
 
   skip_before_action :authenticate_user!, only: :index
+
   def index
     if params[:location]
       @meals = User.where("address iLIKE ?", "%#{params[:location]}%").where.not(latitude: nil, longitude: nil).map { |user| user.meals }.flatten
     else
       @meals = User.where.not(latitude: nil, longitude: nil).map { |user| user.meals }.flatten
+    end
+
+    @markers = @meals.map do |meal|
+      {
+        lat: meal.user.latitude,
+        lng: meal.user.longitude#,
+        # infoWindow: { content: render_to_string(partial: "/flats/map_box", locals: { flat: flat }) }
+      }
     end
   end
 
