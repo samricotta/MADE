ca5ba7e154e8b0528b9dc5f8d523163a960e13e8
Natalia Raffaele <natalia.rafe@gmail.com>
2018-05-29 12:32:40 +0100

    index method not finished and navbar and index view skeleton

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,5 +1,12 @@
 class MealsController < ApplicationController
+
+  skip_before_action :authenticate_user!, only: :index
   def index
+    if params[:location]
+      @meals = User.where("address iLIKE ?", "%#{params[:location]}%").where.not(latitude: nil, longitude: nil).map { |user| user.meals }.flatten
+    else
+      @meals = User.where.not(latitude: nil, longitude: nil).map { |user| user.meals }.flatten
+    end
   end
 
   def show
