8e15f1f102d11cb78248481be7ceea5159c88551
Sam Ricotta <samanthalricotta@gmail.com>
2018-05-29 18:09:58 +0100

    changes complete

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,6 +1,6 @@
 class MealsController < ApplicationController
 
-  skip_before_action :authenticate_user!, only: :index
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     if params[:location]
@@ -16,8 +16,9 @@ class MealsController < ApplicationController
         # infoWindow: { content: render_to_string(partial: "/flats/map_box", locals: { flat: flat }) }
       }
     end
-
+  end
   def show
+    @meal_review = MealReview.new
     @meal = Meal.find(params[:id])
   end
 
