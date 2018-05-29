9f3de93e9b8f52981ab460efe0c2257f4f458597
Sam Ricotta <samanthalricotta@gmail.com>
2018-05-29 14:37:49 +0100

    changes

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,6 +1,6 @@
 class MealsController < ApplicationController
 
-  skip_before_action :authenticate_user!, only: :index, :show
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     if params[:location]
