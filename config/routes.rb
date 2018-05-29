27d666d0466155238f9967812eabae540e8785cc
Sara Foresti <Sara.foresti@hotmail.com>
2018-05-29 17:24:17 +0100

    destroy action works

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,5 @@ Rails.application.routes.draw do
   resources :order, only: :show
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/dashboard', to: 'pages#dashboard'
 end
