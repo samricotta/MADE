340aa95f50c9bdebd91aeefbd76a25672a81cf58
nabil far <nabil.far.contact@gmail.com>
2018-05-29 14:15:15 +0100

    finished new meal form

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -5,9 +5,29 @@ class MealsController < ApplicationController
   def show
   end
 
-  def new
-  end
-
   def edit
   end
+
+  def new
+    @meal = Meal.new
+  end
+
+  def create
+    @meal = Meal.new(meal_params)
+    @meal.user = current_user
+    if @meal.save
+      redirect_to meal_path(@meal)
+    else
+      # raise
+      render :new
+    end
+  end
+
+private
+  def meal_params
+    params.require(:meal).permit(:name, :description, :ingredients, :portions_left, :cuisine, :dietary)
+  end
+
 end
+
+
