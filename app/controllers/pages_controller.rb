d6a5a37793ca221991e837095fc0476fe1690cd1
Sam Ricotta <samanthalricotta@gmail.com>
2018-06-01 11:38:15 +0100

    complete

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,9 +4,11 @@ class PagesController < ApplicationController
   def home
     @top_chef = User.all.sample
     @four_meals = Meal.where(user: @top_chef).first(4)
+    @six_meals = Meal.last(6)
   end
 
   def dashboard
     @meals = current_user.meals
   end
+
 end
