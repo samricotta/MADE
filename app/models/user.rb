3bcc45664e119f507c44458cf1530421d45411a7
Sam Ricotta <samanthalricotta@gmail.com>
2018-06-06 18:25:29 +0100

    complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_many :meals, dependent: :destroy
   has_many :meal_orders, through: :orders
   has_many :messages
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -11,5 +12,22 @@ class User < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
   mount_uploader :photo, PhotoUploader
+
+  def average_rating
+    total = 0
+    number_of_reviews = 0
+    meals.each do |meal|
+      meal.meal_reviews.each do |review|
+        total += review.rating
+        number_of_reviews += 1
+      end
+    end
+    if number_of_reviews == 0
+      return 0
+    else
+      average = total/number_of_reviews
+      return average
+    end
+  end
 
 end
